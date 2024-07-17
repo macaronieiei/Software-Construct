f4483bd84a860b2ea6b0f4078ec375d305fb5c2b
macaronieiei <154341742+macaronieiei@users.noreply.github.com>
2024-07-17 22:07:55 +0700

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,25 +1,28 @@
-package Lap4;
+package lap4;
 
 public class BankAccount {
 	private String account;
 	private double amount;
 	private Date date;
+	public static int count = 1;
 	
 	public BankAccount(String name) {
 		account = name;
 		amount = 0;
 		date = new Date(16, 7, 2024);
+		count++;
 		System.out.println("Create Acount Name : "+account);
 		System.out.println("Amount : "+amount+" Bath");
-		System.out.println("Openning Date : "+date.getterDay()+" "+date.getterMonth()+" "+date.getterYear());
+		System.out.println("Openning Date : "+date.getDay()+" / "+date.getMonth()+" / "+date.getYear());
 	}
 	public BankAccount(String name, double amount, Date date) {
 		this.account = name;
 		this.amount = amount;
 		this.date = date;
+		count++;
 		System.out.println("Create Acount Name : "+account);
 		System.out.println("Amount : "+this.amount+" Bath");
-		System.out.println("Openning Date : "+date.getterDay()+" "+date.getterMonth()+" "+date.getterYear());
+		System.out.println("Openning Date : "+date.getDay()+" / "+date.getMonth()+" / "+date.getYear());
 	}
 	public void deposit(double amount) {
 		this.amount = this.amount+amount;
@@ -37,21 +40,22 @@ public class BankAccount {
 		}
 	}
 	public void tranfer(BankAccount other,double amount) {
-		if(amount<=this.amount && amount>0) {
-			this.amount = this.amount-amount;
+		boolean success = withdraw(amount);
+		if(success) {
 			System.out.println(this.account+" "+amount + " Bath Transfer Success!!!");
 			other.deposit(amount);
 		}
 		else {
-			System.out.println("!! Sorry"+this.account+" Cant Transfer.!!!");
+		System.out.println("!! Sorry"+this.account+" Cant Transfer.!!!");
 		}
 	}
-	public void property() {
-		System.out.println("Current amount : "+this.amount);
+	public double getAmount() {
+		
+		return this.amount;
 	}
 	public String getInfo() {
-		return "Account : " + this.account + "\nOpening Date : " + this.date.getterDay() + " " 
-				+ this.date.getterMonth() + " " + this.date.getterYear();
+		return "Account : " + this.account + "\nOpening Date : " + this.date.getDay() + " / " 
+				+ this.date.getMonth() + " / " + this.date.getYear();
 	}
 	
 }
