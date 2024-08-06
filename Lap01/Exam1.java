a5049152453f040d6b667c494944b995287d576a
macaronieiei <154341742+macaronieiei@users.noreply.github.com>
2024-08-06 21:59:40 +0700

    Add files via upload

---
+++ Lap01/Exam1.java
@@ -0,0 +1,10 @@
+package th.ac.ku.kps.cs;
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lap01/test.java
@@ -0,0 +1,10 @@
+package th.ac.ku.kps.cs;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
